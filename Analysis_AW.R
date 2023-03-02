4130e8886f943631de3f4322b8e0d58325a700d0
Ali Waseem <ali.waseem607@gmail.com>
2023-03-02 14:45:46 +0100

    working on reading tables

--- Analysis_AW.R
+++ Analysis_AW.R
@@ -31,4 +31,28 @@ ReadTSeries <- function(filename, timecolumn="datetime", timeformat="%d.%m.%Y %H
   data
 }
 
-rig <- ReadTSeries("./data/RIG.csv", timecolumn = "Date/time")
+timecolumn <- "Date/time"
+timeformat="%d.%m.%Y %H:%M"
+data <- read.table("./data/RIG.csv", skip=5, header=TRUE, sep=";", check.names=FALSE)
+names(data) <- sub("[ ].*$","",names(data)) # strip units for simplification
+names(data) <- sub("Date/time", "Date/time", names(data), fixed=TRUE)
+data[,"Date/time"] <- as.chron(data[,"Date/time"], timeformat) - 1/24 # end time -> start time
+class(data[1,"Date/time"])
+months(data[1,"Date/time"])
+days(data[1,"Date/time"])
+hours(data[1,"Date/time"])
+minutes(data[1,"Date/time"])
+
+for (i in 1:8760){
+  print(i)
+  weekdays(data[i,"Date/time"])
+}
+data[2,"Date/time"]
+
+data[,"year"] <- years(data[,timecolumn])
+data[,"month"] <- months(data[,timecolumn])
+data[,"day"] <- days(data[,timecolumn])
+data[,"hour"] <- hours(data[,timecolumn])
+data[,"dayofwk"] <- weekdays(data[,timecolumn])
+data[,"daytype"] <- ifelse(data[,"dayofwk"] %in% c("Sat","Sun"), "Weekend", "Weekday")
+data[,"season"] <- Month2Season(unclass(data[,"month"]))
