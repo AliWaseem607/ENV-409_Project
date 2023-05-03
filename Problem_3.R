3f3e53aa8be5aa09a8f6af5054f4477bad031a6c
lorcomi <lorenzo.comi@epfl.ch>
2023-05-03 13:51:24 +0200

    wow I am so ahead of you lol get good

--- Problem_3.R
+++ Problem_3.R
@@ -1,5 +1,5 @@
-library(chron)
 library(tidyverse)
+library(chron)
 
 #Options from Stakahama
 Sys.setlocale("LC_TIME","C")
@@ -40,11 +40,14 @@ Month2Season <- function(month) {
 }
 
 
-LUG<-ReadTSeries("./data/LUG.csv")
-RIG<-ReadTSeries("./data/RIG.csv")
+LUG<-ReadTSeries("./data/LUG_2023-04-16.csv")
+RIG<-ReadTSeries("./data/RIG_2023-04-16.csv")
 
-df <- full_join(cbind(site="LUG", ReadTSeries("./data/LUG.csv")),
-                cbind(site="RIG", ReadTSeries("./data/RIG.csv")))
+df <- full_join(cbind(site="LUG", ReadTSeries("./data/LUG_2023-04-16.csv")),
+                cbind(site="RIG", ReadTSeries("./data/RIG_2023-04-16.csv")))
+
+
+
 
 lf <- df %>%
   gather(variable, value,
@@ -61,7 +64,7 @@ ggplot(lf) +
   facet_grid(variable ~ site, scale = "free_y") +
   geom_boxplot(aes(month, value), outlier.size = 0.5, outlier.shape = 3)
 
-ggplot(lf) +
+ggplot(subset(lf, variable %in% c("EC", "CPC", "TEMP", "PREC", "RAD"))) +
   facet_grid(variable ~ site, scale = "free_y") +
   geom_boxplot(aes(season, value), outlier.size = 0.5, outlier.shape = 3)
 
@@ -70,3 +73,67 @@ lf %>%
   ggplot +
   facet_grid(variable ~ season, scale = "free_y") +
   geom_boxplot(aes(daytype, value), outlier.size = 0.5, outlier.shape = 3)
+
+cat(encodeString(readLines("data/LUG_Wind_MM10_22.txt", n=20)), sep="\n")
+cat(encodeString(readLines("data/RIG_Wind_MM10_22.txt", n=20)), sep="\n")
+
+ReadMet <- function(filename) {
+  data <- read.table(filename, skip=15, col.names=c("year", "month", "day", "hour", "minute", "WIRI", "WIGE"))
+  data %>%
+    mutate(datetime = as.chron(paste(year, month, day, hour, minute), "%Y %m %d %H %M"),
+           year     = years(datetime),
+           month    = months(datetime),
+           day      = days(datetime),
+           hour     = hours(datetime),
+           minute   = minutes(datetime),
+           WIRI     = ifelse(WIRI <= -9999, NA, WIRI),
+           WIGE     = ifelse(WIGE <= -9999, NA, WIGE))
+}
+
+datapath <- "./data/"
+met <- full_join(cbind(site="LUG", ReadMet(file.path(datapath, "LUG_Wind_MM10_22.txt"))),
+                 cbind(site="RIG", ReadMet(file.path(datapath, "RIG_Wind_MM10_22.txt"))))
+
+
+
+mean.angle <- function(theta, r=1, ...) {
+  ## Function for averaging angles
+  ## Polar coordinates -> Cartesian coordinates -> polar coordinates
+  ##   'theta' is in degrees
+  ##   'r=1' for unit circle
+  ##   returns value is mean theta in degrees
+  theta.rad <- theta * pi/180
+  x <- mean(r * cos(theta.rad), ...)
+  y <- mean(r * sin(theta.rad), ...)
+  theta.deg <- atan2(y, x) * 180/pi
+  ifelse(sign(theta.deg) < 0, (theta.deg + 360) %% 360, theta.deg) # -179--180 to 0--359
+}
+
+
+mean(c(359, 1)) # arithmetic mean
+mean.angle(c(359, 1)) # mean in Cartesian coordinates
+
+df1 <- left_join(df,
+                met %>% select(-datetime) %>% mutate(year=as.character(year)))
+# conc[["year"]] is otherwise incompatible with met[["year"]]
+tail(df1)
+
+df1 %>%
+  mutate(hour = factor(hour)) %>%
+  ggplot+
+  facet_grid(site~season)+
+  geom_boxplot(aes(hour, WIGE))+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+source("Windrose.R")
+
+df1 %>%
+  filter(site=="LUG") %>%
+  plotWindrose(spd = "WIGE", dir = "WIRI", decreasing=FALSE)
+
+df1 %>%
+  filter(site=="RIG") %>%
+  plotWindrose(spd = "WIGE", dir = "WIRI", decreasing=FALSE)
+
+plotWindrose(df1, spd = "WIGE", dir = "WIRI") +
+  facet_grid(site~season)
